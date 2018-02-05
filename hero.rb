81c936c36a584de0f9301a936d510d2ac8f3b15c
Rhododendron1210 <lea.louesdon@gmail.com>
2018-02-05 18:13:18 +0100

    preso

---
+++ hero.rb
@@ -0,0 +1,54 @@
+class Hero
+  attr_reader :x, :y
+  # constructeur
+  def initialize(x, y)
+    # coordonnées
+    @x = x
+    @y = y
+    @radius = 10
+    # vitesse (de base 0 : à l'arret)
+    @velocityX = 0.0
+    @velocityY = 1.0
+    #image du personnage
+    @image = Gosu::Image.new("res/hero.png")
+  end
+
+  def draw
+    @image.draw(@x, @y, ZOrder::Hero)
+  end
+
+  # vitesse en x diminue (équivaut à un déplacement vers la gauche)
+  def go_left
+    @velocityX -= 0.5
+  end
+
+  # vitesse en x augmente (équivaut à un déplacement vers la droite)
+  def go_right
+    @velocityX += 0.5
+  end
+
+  # vitesse en y diminue (équivaut à un déplacement vers le haut)
+  def go_up
+    @velocityY -= 25
+    move
+    sleep(1.0/24.0)
+    @velocityY += 25
+  end
+
+  # vitesse en y augmente (équivaut à un déplacement vers le bas)
+  def go_down
+    @velocityY += 0.5
+  end
+
+  # modification des coordonées du héros
+  def move
+    @x += @velocityX
+    @x %= 1024
+    @y += @velocityY
+    @y %= 576
+    @velocityX *= 0.96
+    if @velocityY<1
+    @velocityY *= 0.96
+    end
+  end
+end
